dd72c3efbb4a852c260ea7445cc24cd04438bc6d
Cameron Taggart <cameron.taggart@microsoft.com>
2024-08-11 19:25:08 -0400

    remove web project

--- Aspir8.AppHost/Program.cs
+++ Aspir8.AppHost/Program.cs
@@ -7,8 +7,4 @@ builder.AddContainer("apiContainer", "mcr.microsoft.com/dotnet/aspnet:5.0")
     .WithDockerfile(".")
     .WithReference(api);
 
-builder.AddProject<Projects.Aspir8_Web>("web")
-    .WithExternalHttpEndpoints()
-    .WithReference(api);
-
 builder.Build().Run();
